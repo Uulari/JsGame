839ea29aee7e7b213df7e6ed651d4c44b2681c77
mide <mide@mide-N56VM>
2014-11-15 19:11:07 +0200

    made small developments to the game controller, otherwise nothing.

--- src/main/java/wad/controller/GameController.java
+++ src/main/java/wad/controller/GameController.java
@@ -2,11 +2,23 @@
 package wad.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping (value = "/") //lisää polku
+@RequestMapping (value = "*") //lisää polku
 public class GameController {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String view(Model model){
+        
+        model.addAttribute("greeting", "Do you want to play a game?");
+        //add here the returning of the game instance (?) and the view
+      
+        return "lol";
+    }
+    
     
    
     
