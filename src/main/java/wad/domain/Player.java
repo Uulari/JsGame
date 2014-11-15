a98dbdd81e0462faf2d37cce755d458bfd10028d
Uulari <uulari@gmail.com>
2014-11-15 19:27:00 +0200

    - Player class created

--- src/main/java/wad/domain/Player.java
+++ src/main/java/wad/domain/Player.java
@@ -10,5 +10,24 @@ package wad.domain;
  * @author User
  */
 public class Player {
-    
+
+    private int score;
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
 }
