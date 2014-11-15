6c11d21ca6feb1e7f4e0f5cdcd980262aabdbce9
mide <mide@mide-N56VM>
2014-11-15 18:31:47 +0200

    test commit

--- src/main/java/com/mycompany/jsgame/controller/Controller.java
+++ src/main/java/com/mycompany/jsgame/controller/Controller.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.jsgame.controller;
 
-/**
- *
- * @author User
- */
+
 public class Controller {
+    
+   //moij
     
 }
